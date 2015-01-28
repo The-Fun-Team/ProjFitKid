690fb4fecb68c00df72937ee8b288d8b464d92e1
IsabelJellen <blueribbonij02@gmail.com>
2015-01-28 07:31:58 -0800

    First commit

---
+++ webapp/js/submitLog.js
@@ -0,0 +1,24 @@
+function log(){
+	
+	var eggs=document.forms["proteinForm"]["eggs"].value;
+	var eggNumber = document.getElementById('Results');
+	var test=6;
+	eggNumber.innerHTML = test;
+	/*var ul = document.getElementById('Makes');
+	for (var j = 0; j < data.length; j += 2) {
+	var li = document.createElement('li');
+	li.setAttribute("class", "list-group-item");
+	if (j + 1 < data.length - 1)
+	li.setAttribute("style","border-bottom-width: 0px");
+	li.innerHTML = data[j];
+	ul.appendChild(li);
+	$('#select').empty();
+	var newSelect = document.getElementById('select');
+	for (var i = 0; i < j + 1; i += 2) {
+	var opt = document.createElement("option");
+	opt.value = data[i];
+	newSelect.appendChild(opt);
+	opt.innerHTML = data[i];*/
+	
+	
+}
